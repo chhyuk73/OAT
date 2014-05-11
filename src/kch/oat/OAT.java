54934fbab4603b02c663e0ba6c7fdb89930e2f5d
chhyuk73 <chhyuk73@naver.com>
2014-05-11 18:16:08 +0900

    main.xml에서 리스트뷰 추가 이미지버튼 백그라운드 투명하게 변경

--- src/kch/oat/OAT.java
+++ src/kch/oat/OAT.java
@@ -3,13 +3,22 @@ package kch.oat;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class OAT extends Activity {
+	private ImageButton imgPlus,imgMinus;
+	private ListView listSubject;
+	private Button btnNext;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		imgPlus=(ImageButton)this.findViewById(R.id.imgPlus);
+		imgMinus=(ImageButton)this.findViewById(R.id.imgMinus);
 	}
 
 	@Override
