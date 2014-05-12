1e0a12437607e5c4ed4fcb9b21c4f7196467309b
chhyuk73 <chhyuk73@naver.com>
2014-05-12 22:40:30 +0900

    Next1 이라는 액티비티파일 추가...

---
+++ src/kch/oat/Next1.java
@@ -0,0 +1,16 @@
+package kch.oat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Next1 extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
