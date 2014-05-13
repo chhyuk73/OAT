e35ff26a880707788e188576b4d569d5042ca878
chhyuk73 <chhyuk73@naver.com>
2014-05-13 19:35:51 +0900

    Activity1이라는 액티비티 추가...

--- src/kch/oat/Activity1.java
+++ src/kch/oat/Activity1.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Activity1 extends Activity {
-
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
