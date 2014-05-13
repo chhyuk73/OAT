2fcef7d62a07b51a4ba1cae9dd176cc83fc7ef13
sbjwin <sbjwin4271@gmail.com>
2014-05-13 12:58:10 +0900

    찬혁아 소스트리 샘이 clone하고 elipse에 연동시켜놨고 지금 commit중이다...

--- src/kch/oat/Next1.java
+++ src/kch/oat/Next1.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 public class Next1 extends Activity {
 
 	/** Called when the activity is first created. */
+	// 일단 추가하는 것만 했구나...
+	// 5/23일에 볼 때 다시 얘기하자...
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	
-	    // TODO Auto-generated method stub
 	}
 
 }
