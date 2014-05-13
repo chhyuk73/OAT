b095f5d9032fae2999df0ba9cff9de6168bdfe05
chhyuk73 <chhyuk73@naver.com>
2014-05-13 19:29:33 +0900

    Activity1 이라는 액티비티 추가...(아직 자세한것은 미완성)

--- src/kch/oat/Activity1.java
+++ src/kch/oat/Activity1.java
@@ -3,13 +3,13 @@ package kch.oat;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Next1 extends Activity {
+public class Activity1 extends Activity {
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    super.setContentView(R.layout.next1);
+	    super.setContentView(R.layout.activity1);
 	
 	    // TODO Auto-generated method stub
 	}
