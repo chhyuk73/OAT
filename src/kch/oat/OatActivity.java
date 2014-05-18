6e0284047693e0e1a332bfec9736fa5ad9096352
chhyuk73 <chhyuk73@naver.com>
2014-05-18 17:50:49 +0900

    TabSpec에 대한 커멘트를 달았다.

--- src/kch/oat/OatActivity.java
+++ src/kch/oat/OatActivity.java
@@ -1,22 +1,37 @@
 package kch.oat;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
 import android.widget.TabHost;
 
 public class OatActivity extends Activity {
 
-	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    super.setContentView(R.layout.oat_activity);
-	    
-	    TabHost tHost=(TabHost)this.findViewById(R.id.tabhost);
-	    tHost.setup();
-	    
-	    TabHost.TabSpec tSpec=tHost.newTabSpec("tab1");
-	    //tSpec.setContent(R)
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_oat);
+		
+		// TabHost 积己
+		TabHost tHost=(TabHost)this.findViewById(R.id.tabhost);
+		tHost.setup();
+
+		TabHost.TabSpec tSpec=tHost.newTabSpec("tab1");
+		tSpec.setContent(R.id.tab1);
+		tSpec.setIndicator("苞切");
+		tHost.addTab(tSpec);
+		
+		// TabSpec 积己
+		tSpec=tHost.newTabSpec("tab2");
+		tSpec.setContent(R.id.tab2);
+		tSpec.setIndicator("荐切");
+		tHost.addTab(tSpec);
+		tHost.setCurrentTab(0);
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.oat, menu);
+		return true;
+	}
 }
