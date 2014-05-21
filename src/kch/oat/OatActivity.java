e49331e6e9e33da63ec8de3790a00b8826465c0b
chhyuk73 <chhyuk73@naver.com>
2014-05-21 16:45:00 +0900

    patch

--- src/kch/oat/OatActivity.java
+++ src/kch/oat/OatActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TabHost;
+import android.widget.TextView;
 
-public class OatActivity extends Activity {
+public class OatActivity extends Activity implements TabHost.TabContentFactory{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +19,14 @@ public class OatActivity extends Activity {
 		TabHost tHost=(TabHost)this.findViewById(R.id.tabhost);
 		tHost.setup();
 
-		TabHost.TabSpec tSpec=tHost.newTabSpec("tab1");
+		TabHost.TabSpec tSpec=tHost.newTabSpec("science");
 		tSpec.setIndicator("과학");
-		tSpec.setContent(new Intent(OatActivity.this,SubjectContentView.class));
+		tSpec.setContent(this);
 		tHost.addTab(tSpec);
 		
 		// TabSpec 생성
-		tSpec=tHost.newTabSpec("tab2");
-		//tSpec.setContent(R.id.tab2);
+		tSpec=tHost.newTabSpec("math");
+		tSpec.setContent(this);
 		tSpec.setIndicator("수학");
 		tHost.addTab(tSpec);
 		tHost.setCurrentTab(0);
@@ -34,5 +36,11 @@ public class OatActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.oat, menu);
 		return true;
+	}
+
+	@Override
+	public View createTabContent(String tag) {
+		SubjectView sv=new SubjectView(this);
+		return sv;
 	}
 }
